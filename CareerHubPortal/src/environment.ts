c3e370cc3d1402c91b46d64276bec005e726c552
subitchan <74167727+subitchan@users.noreply.github.com>
2026-02-11 00:11:53 +0530

    Add files via upload

--- CareerHubPortal/src/environment.ts
+++ CareerHubPortal/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   //http://localhost:2003
-  apiUrl: "http://localhost:2003",
+  apiUrl: "https://careerhub-portal.onrender.com",
   // https://dane-closing-constantly.ngrok-free.app
  };
